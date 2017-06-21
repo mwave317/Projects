b00a57d97ac5925e34e811dcac3ea22e32c0c660
Ken Coppola <mwave317@gmail.com>
2017-06-21 09:54:49 -0400

    Add files via upload

---
+++ daySix/sneakers.js
@@ -0,0 +1,15 @@
+$("button").click(function() {
+    $("button").css("color", "white").css("background", "none").css("border-color", "white").text("ADDING TO CART");
+    $('button').animate({'opacity': 1}, 300, function () {
+    $(this).text('ADDED TO CART');
+}).animate({'opacity': 1});
+});
+$("button").click(function() {
+ let cart= parseInt($('p.items').val()*1+ 1);
+    $("p.items").text("("+cart+")");
+    parseInt($('p.items').val(cart)*1+ 1)
+});
+$("#details").click(function() {
+    $("#saying").hide();
+});
+    
